07f11045cec1e0fb0269983b06f19d19227dd89c
Myles Motion <TCMylesM@users.noreply.github.com>
2020-05-29 14:11:07 +0100

    changed offline check

--- inc/serverinfo.php
+++ inc/serverinfo.php
@@ -4,8 +4,7 @@ $json = 'https://mcapi.us/server/status?ip=play.spleefleague.com';
 $content = file_get_contents($json);
 $json = json_decode($content, true);
 
-
-if (isset($json['online'])){
+if ($json['online'] == true){
     $online = "Players Online: " . $json['players']['now'];
 } else {
     $online = "The server is currently offline!";
